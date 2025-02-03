d256b590b912002e869be7d9ebeb393591567611
Kim Faint <kim.faint@gmail.com>
2025-02-03 19:40:43 +1000

    No need to start VirtioFsSvc during install

--- local/winfsp.ps1
+++ local/winfsp.ps1
@@ -3,7 +3,6 @@
 echo "Enable the VirtIO File System Service"
 $serviceName = 'VirtioFsSvc'
 Set-Service -Name $serviceName -StartupType 'Automatic'
-Start-Service -Name $serviceName
 
 echo "Download and install WinFSP"
 Invoke-WebRequest -Uri 'https://github.com/winfsp/winfsp/releases/download/v2.0/winfsp-2.0.23075.msi' -OutFile 'C:\winfsp-2.0.23075.msi'
