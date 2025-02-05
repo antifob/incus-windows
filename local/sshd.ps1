0c06da51e1caa54b67c9bb13bd3b5369aa1fec34
Kim Faint <kim.faint@gmail.com>
2025-02-05 15:59:26 +1000

    Don't worry about starting service, ahcnge comments to echos

--- local/sshd.ps1
+++ local/sshd.ps1
@@ -1,14 +1,8 @@
-# Install OpenSSH Server if not already installed
+echo "Install OpenSSH Server if not already installed"
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-# Start the SSHD service
-Start-Service sshd
-
-# Set the SSHD service to start automatically
+echo "Set the SSHD service to start automatically"
 Set-Service -Name sshd -StartupType 'Automatic'
 
-# Confirm the SSHD service is running
-Get-Service -Name sshd
-
-# Optionally, configure the firewall to allow SSH traffic
+echo "Configure the firewall to allow SSH traffic"
 New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
