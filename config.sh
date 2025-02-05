85c91e19c649b07b758a9eaf7d46cff4a496ec02
Philippe Grégoire <git@pgregoire.xyz>
2025-02-05 14:18:30 -0500

    virtio-win: upgrade to 0.1.240-1
    Tested with: 2008, 2012, 2016, 2019, 2022, 10e (22h2, 21h2, 20h2)

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 VIRTIO_ARCHIVE="${VIRTIO_ARCHIVE:-https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/}"
-VIRTIO_VERSION="${VIRTIO_VERSION:-0.1.229-1}"
-VIRTIO_SHA256="${VIRTIO_SHA256:-c88a0dde34605eaee6cf889f3e2a0c2af3caeb91b5df45a125ca4f701acbbbe0}"
+VIRTIO_VERSION="${VIRTIO_VERSION:-0.1.240-1}"
+VIRTIO_SHA256="${VIRTIO_SHA256:-ebd48258668f7f78e026ed276c28a9d19d83e020ffa080ad69910dc86bbcbcc6}"
